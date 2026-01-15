066f0dc46dc4fc0e1ef7c2ed39d80613302f011a
fribeiro <flavioxconsult@gmail.com>
2026-01-15 18:05:53 -0300

    classes e métodos Abstratos

---
+++ 2. POO - programação orientada a objetos/1.classes-objetos-propriedades-metodos-encapsulamento/5.outros-metodos-importantes/1.classes-e-metodos-abstratos.php
@@ -0,0 +1,82 @@
+<?php
+
+// Não faz sentido Instanciar a Classe ANIMAL, por se tratar apenas de uma "Classe Conceitual".
+// -- Nesse caso, é recomendável defini-la como uma CLASSE ABSTRATA (que não poderá ser instanciada diretamente).
+abstract class Animal {
+    abstract public function emitirSom();
+    // ---- E a partir da definição de um MÉTODO ABSTRATO, isso garante que todas suas Classes Filhas tenham esse método implementado na sua definição (mesmo que retornem um valor "nulo").
+}
+
+// $animal = new Animal;
+// var_dump($animal);
+// echo($animal->emitirSom()."<br>");
+
+class Dinossauro extends Animal {       // Implementação obrigatória do método EMITIR-SOM!
+    public $nome = "dino";
+    public function emitirSom()
+    {
+        return 'ZZZZZZZZZZZ <br>';
+    }
+};
+
+
+
+$dino = new Dinossauro;
+var_dump($dino);
+echo("O dinossauro {$dino->nome} diz {$dino->emitirSom()}");
+
+// ================================================
+
+class Peixe extends Animal {
+    public $nome = 'peixe';
+    public function emitirSom()
+    {
+        return "{$this->nome} diz gluglu";
+    }
+}
+class Cachorro extends Animal {
+    public $nome="cachorro";
+    public function emitirSom()
+    {
+        return "O {$this->nome} diz Au Au <br>";
+    }
+}
+class FilaBrasileiro extends Cachorro {
+    public $nome = "fila";
+    public function emitirSom()
+    {
+        return "{$this->nome} faz Ruff Ruff <br>";
+    }
+}
+class Gato extends Animal {
+    public $nome = "gato";
+    public function emitirSom()
+    {
+        return "O {$this->nome} faz Miau <br>";
+    }
+}
+
+$cachorro = new Cachorro;
+// print($cachorro->emitirSom());
+
+$gato = new Gato;
+// print($gato->emitirSom());
+
+$fila = new FilaBrasileiro;
+// print($fila->emitirSom());
+// print("Cachorro faz {$cachorro->emitirSom()} e o Fila faz {$fila->emitirSom()}");
+
+$peixe = new Peixe;
+
+// =======================================
+
+function fazerOAnimalEmitirSom($animal) {
+    echo $animal->emitirSom();
+}
+
+// $animais = [$cachorro, $gato, $fila];
+$animais = array($cachorro, $gato, $fila, $peixe);
+foreach($animais as $animal) {
+    print($animal->emitirSom());
+    // fazerOAnimalEmitirSom($animal);
+}
